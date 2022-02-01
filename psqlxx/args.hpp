6dfe4a65df019448db93cc852a8fe8e5e01a1dc6
Yang Yang <yyang.pplus@gmail.com>
2022-01-31 21:40:57 -0800

    Args: Update code to cxxopts 3.0

--- psqlxx/args.hpp
+++ psqlxx/args.hpp
@@ -10,7 +10,7 @@ cxxopts::Options CreateBaseOptions();
 
 [[nodiscard]]
 std::optional<cxxopts::ParseResult>
-ParseOptions(cxxopts::Options &options, int argc, char **argv) noexcept;
+ParseOptions(cxxopts::Options &options, int argc, const char *argv[]) noexcept;
 
 void HandleBaseOptions(const cxxopts::Options &options,
                        const cxxopts::ParseResult &parsed_options);
